3020f81e8c50bdce96d180123adaacbe9c1ce953
David Pritchard <david.al.pritchard@gmail.com>
2021-03-20 14:26:44 -0400

    add dp-selected-text-or-word

--- lisp/ess+/ess-utils.el
+++ lisp/ess+/ess-utils.el
@@ -3,13 +3,24 @@
 (defun dp-ess-eval-word ()
   "Evaluate the word at point in the inferior R process."
   (interactive)
-  (ess-eval-linewise (dp-ess-word-at-point-to-string)))
+  (ess-eval-linewise (dp-selected-text-or-word)))
 
+(defun dp-ess-str-word ()
+  "Get the 'str' of the word at point in the inferior R process."
+  (interactive)
+  (let* ((section (dp-selected-text-or-word))
+         (str-call (concat "str(" section ")")))
+    (ess-eval-linewise str-call)))
 
 (defun dp-ess-word-at-point-to-string ()
   "Find the word at point and return it as a string."
   (save-excursion
     (skip-chars-forward " \t\n")
-    (buffer-substring
+    (buffer-substring-no-properties
      (+ (point) (skip-chars-backward "a-zA-Z0-9._"))
      (+ (point) (skip-chars-forward "a-zA-Z0-9._")))))
+
+(defun dp-selected-text-or-word ()
+  (if (use-region-p)
+      (buffer-substring-no-properties (mark) (point))
+    (dp-ess-word-at-point-to-string)))
