43511c032a3cfa341d348d4602e74498e3b76713
David Pritchard <david.al.pritchard@gmail.com>
2023-06-19 13:04:23 -0400

    add 'dp-org-clip-sharepoint'

--- lisp/org-utilities/org-external-link-sharepoint.el
+++ lisp/org-utilities/org-external-link-sharepoint.el
@@ -1,10 +1,26 @@
 ;;; lisp/org-utilities/org-external-link-sharepoint.el -*- lexical-binding: t; -*-
 
+;; Example data:
 ;; https://targetpharmasolutions.sharepoint.com/:x:/r/sites/NoviSci2/Shared%20Documents/Operations/2023%20Effort%20Planning/Effort%20Planning.xlsm?d=wf4d990570ef5417e99ffb3bfafcb4bc6&csf=1&web=1&e=bridvl
 
 (require 'url)
+(require 'org-cliplink)
 
-(defun dp-extract-filename-from-url (url-string)
+(defun dp-org-clip-sharepoint ()
+  "Create an Org link from a SharePoint clipboard link."
+  (interactive)
+  (let* ((url-string (org-cliplink-clipboard-content))
+         (sharepoint-path (dp-extract-sharepointpath-from-url url-string))
+         (link-string (format "[[%s][%s]]"
+                              url-string
+                              sharepoint-path)))
+    (insert link-string)))
+
+;; TODO: extract `prefix' as a customizeable parameter
+;; TODO: throw error if the `:type' returned by `url-generic-parse-url' isn't
+;; ="https"=?
+(defun dp-extract-sharepointpath-from-url (url-string)
+  "Extract the Sharepoint path from a SharePoint URL string."
   (let* ((parsed-url (url-generic-parse-url url-string))
          (filename (url-filename parsed-url))
          (unhexed-filename (url-unhex-string filename))
