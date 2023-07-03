0c6d3189d4fda31bfdcb582baa0f3ae69cfe5efb
David Pritchard <david.al.pritchard@gmail.com>
2023-07-03 09:59:50 -0400

    updates to org-agenda-knowledgebase-open.el

--- lisp/org-utilities/org-agenda-knowledgebase-open.el
+++ lisp/org-utilities/org-agenda-knowledgebase-open.el
@@ -1,34 +1,33 @@
 ;;; lisp/org-utilities/org-agenda-knowledgebase-open.el -*- lexical-binding: t; -*-
 
-;; ;; Potential plan: set org-agenda-after-show-hook and call org-agenda-switch-to
-;; (defun dp-org-agenda-knowledgebase-open ()
-;;   (interactive)
-;;   (let (())))
-
 (defun dp-knowledgebase-open-or-follow-link ()
+  "Open the Org Attach attachment or follow the 'URL' property link
+
+First check whether the current Org headline has an Org Attach
+property, and if so then attempt to open the corresponding
+file(s). Otherwise, attempt to follow the 'URL' property link."
+  (interactive)
   (if (dp-org-check-attach)
       (org-attach-open)
-    ))
+    (dp-knowledgebase-follow-link)))
 
 (defun dp-org-check-attach ()
-  "Check whether the current headline has an Org Attach attachment"
+  "Check whether the current headline has an Org Attach property"
   (let* ((tags (org-get-tags))
          (has-attach-tag (member "ATTACH" tags)))
     has-attach-tag))
 
 (defun dp-knowledgebase-follow-link ()
+  "Follow the 'URL' property link"
   (let ((link-or-url (org-entry-get nil "URL" 'selective)))
     (unless link-or-url
       (error "No 'URL' property found"))
-    ))
-
-(org-link-open-from-string "[[https://targetpharmasolutions.sharepoint.com/:x:/r/sites/NoviSci2-StandardizedPHRs/Shared%20Documents/P0076%20-%20Market%20Insights/01_Atopic%20Dermatitis/SAP%20and%20VSF/AD_Test_Plan.xlsx?d=w1f6da75ffec34fa3aae1d7803da38d4b&csf=1&web=1&e=VzZbmC][P0076 - Market Insights > 01_Atopic Dermatitis > SAP and VSF > AD_Test_Plan.xlsx]]")
-
-;; (if (org-link-p "[[https://example.com][Example Link]]")
+    (org-link-open-from-string link-or-url)))
 
 (defun dp-org-link-p (s)
+  "Check whether the string `S' is an Org link"
   (unless (stringp s)
     (error "'s' must be a string"))
-  (let* ((link-re "\[\[.+\]\]\[.+\]")
+  (let* ((link-re "\\[\\[.+\\]\\[.+\\]\\]")
          (is-link (string-match link-re s)))
     is-link))
