9d6d70e3c2dc0eb6cb79b760a8a600197c91ff29
David Pritchard <david.al.pritchard@gmail.com>
2024-01-01 09:00:58 -0500

    add org-roam-utilities.el

---
+++ lisp/org-roam-utilities/org-roam-utilities.el
@@ -0,0 +1,26 @@
+;;; lisp/org-roam-utilities/org-roam-utilities.el -*- lexical-binding: t; -*-
+
+;; This is nearly the same as the `"knowledgebase"' entry in the
+;; `org-roam-capture-templates' defined in the config. Should we just use that
+;; one?
+(defun dp-capture-knowledgebase ()
+  (interactive)
+  (let (;; (node (org-roam-node-create :id "7726ded9-86fe-47b5-910e-6afd46c46fef"))
+        ;; (node (org-roam-node-create :file-title "knowledgebase"))
+        (node (org-roam-node-create))
+        (templates `(("k"
+                      "knowledgebase"
+                      entry
+                      "* %?"
+                      ;; "* ${myfoo}%?"
+                      ;; "* TODO %?\n%(yas-expand-snippet (yas-lookup-snippet \"todo\" 'org-mode))\n"
+                      :target (file ,dp-knowledgebase-path)))))
+    (org-roam-capture- :node node :templates templates)))
+
+(defun dp-expand-snippet-knowledgebase ()
+  (when (string= (dp-get-indirect-file-buffer)
+                 dp-knowledgebase-path)
+    (yas-expand-snippet (yas-lookup-snippet "knowledgebase" 'org-mode))))
+
+(defun dp-get-indirect-file-buffer ()
+  (buffer-file-name (or (buffer-base-buffer) (current-buffer))))
