074b472c41314066c46fcdb1ca243d934c675fde
David Pritchard <david.al.pritchard@gmail.com>
2024-01-01 09:01:42 -0500

    update custom.el

--- custom.el
+++ custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
-   '((elisp-lint-indent-specs
+   '((haskell-process-args-cabal-repl "time-to-event-test" "--ghc-option=-ferror-spans")
+     (elisp-lint-indent-specs
       (describe . 1)
       (it . 1)
       (thread-first . 0)
