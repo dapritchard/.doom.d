73aad1b23322686848ae77a1e671a6ef0d76ae73
David Pritchard <david.al.pritchard@gmail.com>
2024-01-01 08:46:59 -0500

    add 'dp-org-create-inactive-timestamp-now'

--- lisp/org-utilities/org-create-inactive-timestamp-now.el
+++ lisp/org-utilities/org-create-inactive-timestamp-now.el
@@ -1,6 +1,10 @@
 ;;; lisp/org-utilities/org-create-inactive-timestamp-now.el -*- lexical-binding: t; -*-
 
-(defun dp-org-create-inactive-timestamp-now (_)
-  ""
-  (let ((time (format-time-string "[%Y-%m-%d %a %H:%M]")))
-    (org-set-property "CREATION_TIMESTAMP" time)))
+(defun dp-org-create-inactive-timestamp-now ()
+  "Create a string representing the current time
+The string is presented in the format of an Org mode inactive timestamp"
+  (format-time-string "[%Y-%m-%d %a %H:%M]"))
+
+;; (defun dp-org-set-inactive-timestamp-property (_)
+;;   (let ((time (dp-org-create-inactive-timestamp-now)))
+;;     (org-set-property "CREATION_TIMESTAMP" time)))
